007d67c57bfc5927e732a0de02dd1c176a34aefc
kai <puahyikai@gmail.com>
2024-05-30 12:39:31 +0800

    modify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack : (config) => {
-        config.external.push({
+        config.externals.push({
             "utf-8-validate": "commonjs utf-8-validate",
             "bufferutil": "commonjs bufferutil",
             canvas: "commonjs canvas",
